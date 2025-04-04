b74e6a6a9aa7b603f524c93356d36826393f1a67
ViictorHugoo <victorhugorodriguesguimaraes@gmail.com>
2025-04-03 22:56:47 -0300

    refactor: refatorado para igualar ao csv

--- etapa3/ddl-3.3.sql
+++ etapa3/ddl-3.3.sql
@@ -3,7 +3,6 @@ CREATE DATABASE
     
 USE intuitive; 
 
-
 CREATE TABLE OPERADORAS_ATIVAS (
     registro_operadora VARCHAR(6) NOT NULL,
     cnpj VARCHAR(14) NOT NULL,
@@ -22,42 +21,18 @@ CREATE TABLE OPERADORAS_ATIVAS (
         'Regiao 6'
     ) NOT NULL,
     data_registro_ans DATE NOT NULL,
-    CONSTRAINT PK_OPERADORAS_ATIVAS PRIMARY KEY (registro_operadora)
-) ENGINE = InnoDB;
-
-
-CREATE TABLE ENDERECOS_OPERADORA (
-    registro_operadora VARCHAR(6) NOT NULL,
-    logradouro VARCHAR(40),
+	logradouro VARCHAR(40),
     numero VARCHAR(20) NOT NULL,
     complemento VARCHAR(40),
     bairro VARCHAR(30),
     cidade VARCHAR(30),
     uf CHAR(2),
     cep VARCHAR(8) NOT NULL,
-    PRIMARY KEY (cep, numero),
-    CONSTRAINT FK_ENDERECOS_OPERADORA_OPERADORAS_ATIVAS 
-        FOREIGN KEY (registro_operadora) 
-        REFERENCES OPERADORAS_ATIVAS(registro_operadora)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE = InnoDB;
-
-
-CREATE TABLE TELEFONES_OPERADORA (
-    id INT AUTO_INCREMENT,
-    registro_operadora VARCHAR(6) NOT NULL,
-    telefone VARCHAR(20),
+	telefone VARCHAR(20),
     ddd VARCHAR(4),
     fax VARCHAR(20),
-    CONSTRAINT PK_TELEFONES_OPERADORA PRIMARY KEY (id),
-    CONSTRAINT FK_TELEFONES_OPERADORA_OPERADORAS_ATIVAS 
-        FOREIGN KEY (registro_operadora) 
-        REFERENCES OPERADORAS_ATIVAS(registro_operadora)
-        ON DELETE CASCADE
-        ON UPDATE CASCADE
-) ENGINE = InnoDB AUTO_INCREMENT = 1;
-
+    CONSTRAINT PK_OPERADORAS_ATIVAS PRIMARY KEY (registro_operadora)
+) ENGINE = InnoDB;
 
 CREATE TABLE DEMONSTRACOES_CONTABEIS (
     registro_operadora VARCHAR(6) NOT NULL,
@@ -73,5 +48,3 @@ CREATE TABLE DEMONSTRACOES_CONTABEIS (
         ON DELETE CASCADE
         ON UPDATE CASCADE
 ) ENGINE = InnoDB;
-
-
